9ebbed2eaed9b917636a508b281529522fe8a73b
EddiePeng <33013483+Eddie1874@users.noreply.github.com>
2021-03-20 21:33:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,5 +11,5 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 sed -i 's/0x7b0000/0x1fb0000/' target/linux/ramips/dts/mt7620a_phicomm_psg1218.dtsi
